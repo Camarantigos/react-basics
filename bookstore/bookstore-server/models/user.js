8e8001d4ea114213b0c0239799013590049972c1
Charis Amarantigos <harisamarad@gmail.com>
2019-12-07 15:44:29 +0200

    user schema salted

--- bookstore/bookstore-server/models/user.js
+++ bookstore/bookstore-server/models/user.js
@@ -21,6 +21,31 @@ const userSchema = new mongoose.Scheema({
     }
 });
 
+userSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+        // Hash diff each time and wait until it finishes
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        // then save it
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+// check is correct password is given
+userSchema.method.comparePassword = async function (candidatePassword, next) {
+    try {
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (err) {
+        return next(err);
+    }
+}
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
