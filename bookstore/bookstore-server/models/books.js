e7a24706be616b2d2419ee57099c2d82fa522fd5
Charis Amarantigos <harisamarad@gmail.com>
2019-12-07 15:33:04 +0200

    books

--- bookstore/bookstore-server/models/books.js
+++ bookstore/bookstore-server/models/books.js
@@ -1,22 +1,46 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Scheema({
-    email: {
+    title: {
         type: string,
         required: true,
-        unique: true
+        match: /^[ A-Za-z0-9@”#&*!]*$/
     },
-    username: {
-        type: string,
-        required: true,
-        unique: true
-    },
-    password: {
+    description: {
         type: string,
         required: true
     },
-    profileImage: {
-        type: String
+    category: [{
+        type: string,
+        required: true
+    }],
+    authorNames: [{
+        type: String,
+        required: true
+        // console.log(typeof req.body.authorNames)
+    }],
+    publisher: {
+        type: string,
+        required: true
+    },
+    year: {
+        type: string,
+        required: true
+    },
+    numberOfPages: {
+        type: number,
+        required: true,
+        max: 9999
+    },
+    isbn10: {
+        type: string,
+        required: true,
+        maxlength: 10
+    },
+    isbn13: {
+        type: string,
+        required: true,
+        maxlength: 13
     }
 });
 
