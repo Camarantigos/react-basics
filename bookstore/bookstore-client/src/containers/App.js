a7b77ac2d5f88ea188bf9ea8473460efc8165070
Charis Amarantigos <harisamarad@gmail.com>
2019-12-08 18:13:28 +0200

    hello world front

--- bookstore/bookstore-client/src/containers/App.js
+++ bookstore/bookstore-client/src/containers/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "../store";
+import { BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>Hello World!</div>
+    </Router>
+  </Provider>
+);
 
 export default App;
