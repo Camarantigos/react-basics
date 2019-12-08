5e72be951e52045b76e00af50219fecb8bcbeea6
Charis Amarantigos <harisamarad@gmail.com>
2019-12-09 00:02:59 +0200

    form main build

---
+++ bookstore/bookstore-client/src/components/AuthForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+export default class AuthForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            username: "",
+            password: "",
+            profileImageUrl: "",
+        };
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    render() {
+        const { email, username, password, profileImageUrl } = this.state;
+        const { heading, buttonText, signUp } = this.props;
+        return (
+            <div>
+                <div className="row justify-content-md-center text-center text-light">
+                    <div className="col-md-6">
+                        <form onSubmit={this.handleSubmit}>
+                            <h2>{heading}</h2>
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="email"
+                                name="email"
+                                onChange={this.handleChange}
+                                value={email}
+                            />
+                            <label htmlFor="password">Password:</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                name="password"
+                                value={password}
+                                onChange={this.handleChange}
+                            />
+                            {signUp && (
+                                <div>
+                                    <label htmlFor="username">Username:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="username"
+                                        name="username"
+                                        onChange={this.handleChange}
+                                        value={username}
+                                    />
+                                    <label htmlFor="image-url">
+                                        Image-Url:
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="image-url"
+                                        name="profileImageUrl"
+                                        value={profileImageUrl}
+                                        onChange={this.handleChange}
+                                    />
+                                </div>
+                            )}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- bookstore/bookstore-client/src/containers/Main.js
+++ bookstore/bookstore-client/src/containers/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
 
 const Main = props => {
     return (
@@ -11,6 +12,33 @@ const Main = props => {
                     exact
                     path="/"
                     render={props => <Homepage {...props} />}
+                />
+                <Route
+                    exact
+                    path="/signin"
+                    render={props => {
+                        return (
+                            <AuthForm
+                                buttonText="Log In"
+                                heading="Welcome Back!"
+                                {...props}
+                            />
+                        );
+                    }}
+                />
+                <Route
+                    exact
+                    path="/signup"
+                    render={props => {
+                        return (
+                            <AuthForm
+                                signUp
+                                buttonText="SIgn me up!"
+                                heading="Get the Knowledge you deserve!"
+                                {...props}
+                            />
+                        );
+                    }}
                 />
             </Switch>
         </div>
